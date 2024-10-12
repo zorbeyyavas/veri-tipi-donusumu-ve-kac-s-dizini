282ff82f8e33f597f5b9f4434fc1200b6a437347
zorbeyyavas <yavaszorbey@gmail.com>
2024-10-12 16:02:08 +0300

    Add files via upload

---
+++ veritipidonusum.py
@@ -0,0 +1,8 @@
+#pythonda veri tipi dönüşümleri
+a=23
+print(str(a))
+print(float(a))
+print(float("34"))
+
+#kaçış dizinidir kodun ortasına koyduğum ""ları metine dahil eder ve kod sorunsuz çalışır
+print("gökhan \"yarın gidiyorum\" dedi")
